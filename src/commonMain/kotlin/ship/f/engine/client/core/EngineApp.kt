e9678a670a516b4c94aaf38f7f7bf522efe196ac
Samuel Taiwo <samueltaiwo@stdev.co.uk>
2026-01-02 15:23:27 +0000

    [Cleanup] Removed all references to version 0.2 leaving only version 0.3 code

--- src/commonMain/kotlin/ship/f/engine/client/core/EngineApp.kt
+++ src/commonMain/kotlin/ship/f/engine/client/core/EngineApp.kt
@@ -1,7 +1,7 @@
 package ship.f.engine.client.core
 
 import androidx.compose.runtime.Composable
-import ship.f.engine.client.utils.serverdrivenui2.ext.BlockingLaunchedEffect2
+import ship.f.engine.client.utils.serverdrivenui3.ext.BlockingLaunchedEffect2
 import ship.f.engine.shared.core.Config
 import ship.f.engine.shared.core.Engine.init
 import ship.f.engine.shared.core.ScopedEvent
